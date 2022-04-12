f2819fa95356e138b0d969d7a4fe6ce111b03e06
TomiWebsite <tomicatwebsite@gmail.com>
2022-04-11 20:39:50 -0500

    Ventana e importaciones de modulos utiles

--- reproductor.py
+++ reproductor.py
@@ -1,26 +1,14 @@
+import tkinter as tk #Esta libreria nos permite crear interfaces
 from pygame import mixer #Importamos el modulo para que sea mas facil de utilizar las herramientas
+import mutagen #Permite obtener la duración de la canción
+import os #Este nos proporciona funciones para crear y eliminar un directorio, recuperar su contenido, etc.
+import fnmatch #Este módulo proporciona soporte para comodines de estilo shell de Unix, nos ayuda a identificar las extenciones en consola
 mixer.init()#Iniciamos el reproductor
-cancion = str(input("Selecione la canción "))#Apartir de aqui modificamos los parametros ;)
-mixer.music.load(cancion)#Escribimos el nombre de la canción junto con la extención
 mixer.music.set_volume(0.7)
-mixer.music.play()
-while True:#Hacemos esta condición para que nos pueda dar siempre una opcción
-    print("Pulsa p para pausar la canción")
-    print("Pulsa r para reproducir la canción")
-    print("Pulsa e para elegir otra canción")
-    print("Pulsa s para salir")
-    opccion = input(">>>>>> ")#Ingresamos las opcciones
-    if opccion == "p":
-        mixer.music.pause()#Pausamos musica
-    elif opccion == "r":
-        mixer.music.unpause()#Reproducimos musica
-    elif opccion == "s":
-        mixer.music.stop()#Cerramos la musica y nos salimos del menu de opcciónes
-        exit()
-    elif opccion == "e":#Configuramos para la siguiente musica
-        mixer.music.stop()
-        cancion = str(input("Selecione la canción"))
-        mixer.music.load(cancion)
-        mixer.music.set_volume(0.7)
-        mixer.music.play()
-#Se que esto es basico, pero almenos me ayuda a entender que me viene en mi futuro
+cancion = ''#Apartir de aqui modificamos los parametros ;)
+canvas = tk.Tk()
+canvas.title("Reproductor de musica de TomiCat")
+canvas.geometry("800x500")
+canvas.config(bg='black')
+canvas.mainloop()
+#Estos son algunos de los modulos que posiblemente me ayuden, pero almenos hice mi primera ventana xd
