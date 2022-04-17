b67da99a6a31ffba2c5b05ae754b78a1d88e0121
TomiWebsite <tomicatwebsite@gmail.com>
2022-04-17 08:26:30 -0500

    Mejorando los botones

--- reproductor.py
+++ reproductor.py
@@ -100,7 +100,6 @@ def mas_volumen():
 #Configuración principal de la ventana de windos
 ventana = tk.Tk()
 ventana.title("Reproductor de musica de TomiCat")
-ventana.attributes("-alpha",0.94)
 ventana.geometry("820x550")
 ventana.iconbitmap('icons/ico.ico')
 ventana.resizable(0,0)
@@ -108,7 +107,7 @@ ventana.resizable(0,0)
 #Cajas
 caja1 = Frame(ventana, bg='#F7F5F2',width=850,height=400)#Aqui se muestra la imagen y la lista
 caja1.grid(row=0,column=0,sticky='nsew')
-caja2= Frame(ventana, bg='#F7F5F2',width=850,height=150)#Aqui los controles
+caja2= Frame(ventana, bg='#F7F5F2',width=820,height=150)#Aqui los controles
 caja2.grid(row=1,column=0,sticky='nsew')
 
 #Pestañas de configuración
@@ -124,13 +123,13 @@ menu.add_cascade(label='Carpeta', menu=Carpeta)
 scrollbar = ttk.Scrollbar(caja1, orient=tk.VERTICAL)
 lista = tk.Listbox(caja1, fg="black", bg='#F7F5F2',width=51, height=22, font=('Roboto',11),borderwidth=0,yscrollcommand=scrollbar.set)
 lista.grid(row=0,column=1)
-img = Image.open('rc.jpg')
+img = Image.open('./icons/rc.jpg')
 img= ImageTk.PhotoImage(img)
 label_img = tk.Label(caja1, image=img,height=400, width=400)
 label_img.grid(row=0,column=0)
 
 #Estilos que hacen la diferencia y/o volumen proximamente xd
-espacio = tk.Label(caja2, text='',bg='#F7F5F2',fg='black', font=('Roboto',14),width=52)
+espacio = tk.Label(caja2, text='',bg='#F7F5F2',fg='black', font=('Roboto',14),width=55)
 espacio.grid(row=0,column=2,columnspan=2,pady=10)
 tiempo_label = tk.Label(caja2, text='',bg='#F7F5F2',fg='black', font=('Roboto',14))
 tiempo_label.grid(row=0,column=4,columnspan=2,pady=10)
@@ -138,23 +137,22 @@ tiempo_label.grid(row=0,column=4,columnspan=2,pady=10)
 prev_img = tk.PhotoImage(file='icons/prev.png')
 next_img = tk.PhotoImage(file='icons/next.png')
 play_img = tk.PhotoImage(file='icons/play.png')
-pause_img = tk.PhotoImage(file='icons/stop.png')
-stop_img = tk.PhotoImage(file='icons/pause.png')
+stop_img = tk.PhotoImage(file='icons/stop.png')
 
 #Creamos los botones con los estilos generales
-previous = tk.Button(caja2,text='Anterior', image= prev_img, bg='#F7F5F2', borderwidth=0, command=anterior)
+previous = tk.Button(caja2,text='Anterior', image= prev_img, bg='#F7F5F2', borderwidth=0, relief="flat", command=anterior, cursor="hand1")
 previous.grid(pady=10, row=1,column=1)
-stop = tk.Button(caja2, text='Detener', image=stop_img, bg='#F7F5F2', borderwidth=0, command=detener)
+stop = tk.Button(caja2, text='Detener', image=stop_img, bg='#F7F5F2', borderwidth=0, relief="flat", command=detener, cursor="hand1")
 stop.grid(pady=10, row=1,column=3)
-play = tk.Button(caja2, text='Reproducir', image=play_img, bg='#F7F5F2', borderwidth=0, command=reproducir)
+play = tk.Button(caja2, text='Reproducir', image=play_img, bg='#F7F5F2', borderwidth=0, relief="flat", command=reproducir, cursor="hand1")
 play.grid(pady=10, row=1,column=2)
-nextB = tk.Button(caja2, text='Siguiente', image=next_img, bg='#F7F5F2', borderwidth=0, command=siguiente)
+nextB = tk.Button(caja2, text='Siguiente', image=next_img, bg='#F7F5F2', borderwidth=0, relief="flat", command=siguiente, cursor="hand1")
 nextB.grid(pady=10, row=1,column=4)
 volumen_label = tk.Label(caja2,font=('Roboto',12),bg='#F7F5F2')
 volumen_label.grid(row=0,column=0,columnspan=2)
-volumen_buttom_mas = tk.Button(caja2, text='+',font=('Roboto',12),width=2,command=mas_volumen,bg='#EEEEEE')
+volumen_buttom_mas = tk.Button(caja2, text='+',font=('Roboto',16),width=2,command=mas_volumen,bg='#F7F5F2',relief="flat", cursor="hand1")
 volumen_buttom_mas.grid(row=1,column=0,pady=15,padx=15)
-volumen_buttom_menos = tk.Button(caja2, text='-',font=('Retro Gaming',12),width=2,command=menos_volumen,bg='#EEEEEE')
+volumen_buttom_menos = tk.Button(caja2, text='-',font=('Retro Gaming',16),width=2,command=menos_volumen,bg='#F7F5F2',relief="flat", cursor="hand1")
 volumen_buttom_menos.grid(row=1,column=5,pady=15,padx=15)
 
 #Ingresamos todos los archivos .mp3 de la carpeta que declaramos
